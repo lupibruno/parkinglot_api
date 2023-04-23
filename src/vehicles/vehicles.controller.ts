ab44e5ae81a329c0745d4533edb06ea23e07d8b3
Bruno Parrilla Lupi <lupi.bruno@outlook.com>
2023-04-23 16:16:01 -0300

    feat: cria os controllers do projeto

---
+++ src/vehicles/vehicles.controller.ts
@@ -0,0 +1,66 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { VehiclesService } from './vehicles.service';
+import { CreateVehicleDto } from './dto/create-vehicle.dto';
+import { UpdateVehicleDto } from './dto/update-vehicle.dto';
+import { Vehicle } from './entities/vehicle.entity';
+
+@Controller('vehicles')
+export class VehiclesController {
+  constructor(private readonly vehiclesService: VehiclesService) {}
+
+  @Post()
+  create(@Body() createVehicleDto: CreateVehicleDto) {
+    return this.vehiclesService.create(createVehicleDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.vehiclesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.vehiclesService.findOne(+id);
+  }
+
+  @Get()
+  async findAllByEstablishmentId(
+    @Param('establishmentId') establishmentId: number,
+  ): Promise<Vehicle[]> {
+    return await this.vehiclesService.findAllByEstablishmentId(establishmentId);
+  }
+
+  @Post('entry')
+  async registerEntry(
+    @Param('establishmentId') establishmentId: number,
+    @Body() vehicleData: any,
+  ): Promise<Vehicle> {
+    const vehicle = await this.vehiclesService.registerEntry(
+      establishmentId,
+      vehicleData,
+    );
+    return vehicle;
+  }
+
+  @Post(':vehicleId/exit')
+  async registerExit(
+    @Param('establishmentId') establishmentId: number,
+    @Param('vehicleId') vehicleId: number,
+  ): Promise<Vehicle> {
+    const vehicle = await this.vehiclesService.registerExit(
+      establishmentId,
+      vehicleId,
+    );
+    return vehicle;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateVehicleDto: UpdateVehicleDto) {
+    return this.vehiclesService.update(+id, updateVehicleDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.vehiclesService.remove(+id);
+  }
+}
